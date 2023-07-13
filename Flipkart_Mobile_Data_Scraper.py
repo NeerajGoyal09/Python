8d5eb0aa0f982caae1a43c789300056ce581ffab
NeerajGoyal09 <138383637+NeerajGoyal09@users.noreply.github.com>
2023-07-13 13:45:31 +0530

    Update Flipkart_Mobile_Data_Scraper.py

--- Flipkart_Mobile_Data_Scraper.py
+++ Flipkart_Mobile_Data_Scraper.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests 
 from bs4 import BeautifulSoup
 
+# Function to extract data from HTML
 def get_data(list_name, tag_type , class_name) :
     data = box.find_all(tag_type, class_ = class_name)
     for i in data :
@@ -13,6 +14,7 @@ Ram_and_Rom  = []
 Display = []
 Camera = []
 
+# Scraping mobile phone data from Flipkart
 for i in range(1,11):
     url = "https://www.flipkart.com/search?q=mobile+under+50000&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page="+str(i)
 
@@ -36,7 +38,7 @@ for i in range(1,11):
                 Display.append(data)
             elif i == 2 :
                 Camera.append(data)
-
+# Creating a DataFrame and saving as CSV
 df  = pd.DataFrame({"Names": Names , "Prices" : Prices ,"Ram_&_Rom": Ram_and_Rom , "Display" : Display , "Camera" : Camera })
 
 df.to_csv("Mobiles_under_50000.csv")
